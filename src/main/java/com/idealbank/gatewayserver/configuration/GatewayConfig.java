81de8d8eb8f5cc93bb6b6eb9c441b80091e10804
vivekkarli <vivekkarli7@gmail.com>
2024-11-02 14:01:24 +0530

    add http timeout for loans ms

--- src/main/java/com/idealbank/gatewayserver/configuration/GatewayConfig.java
+++ src/main/java/com/idealbank/gatewayserver/configuration/GatewayConfig.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Configuration
@@ -15,20 +17,28 @@ public class GatewayConfig {
         return builder.routes()
                 .route("accounts", r -> r
                         .path("/idealbank/accounts/**")
-                        .filters(f -> f.rewritePath("/idealbank/accounts/(?<segment>.*)","/${segment}")
+                        .filters(f -> f
+                                .rewritePath("/idealbank/accounts/(?<segment>.*)","/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
-                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+                                .circuitBreaker(config -> config
+                                        .setName("accountsCircuitBreaker")
                                         .setFallbackUri("forward:/contact-support")))
                         .uri("lb://ACCOUNTS"))
                 .route("cards", r -> r
                         .path("/idealbank/cards/**")
-                        .filters(f -> f.rewritePath("/idealbank/cards/(?<segment>.*)","/${segment}")
+                        .filters(f -> f
+                                .rewritePath("/idealbank/cards/(?<segment>.*)","/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://CARDS"))
                 .route("loans", r -> r
                         .path("/idealbank/loans/**")
-                        .filters(f -> f.rewritePath("/idealbank/cards/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .filters(f -> f
+                                .rewritePath("/idealbank/loans/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .retry(retryConfig -> retryConfig
+                                        .setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
                         .uri("lb://LOANS"))
                 .build();
     }
