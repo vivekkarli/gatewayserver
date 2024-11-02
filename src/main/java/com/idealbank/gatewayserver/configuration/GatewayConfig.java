54e3e91c136611c6ce998aebc2963a8f5ff5d466
vivekkarli <vivekkarli7@gmail.com>
2024-11-02 15:05:10 +0530

    add rate limiter for loans ms using redis

--- src/main/java/com/idealbank/gatewayserver/configuration/GatewayConfig.java
+++ src/main/java/com/idealbank/gatewayserver/configuration/GatewayConfig.java
@@ -5,11 +5,14 @@ import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -33,7 +36,10 @@ public class GatewayConfig {
                         .path("/idealbank/cards/**")
                         .filters(f -> f
                                 .rewritePath("/idealbank/cards/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .requestRateLimiter(rateLimitConfig -> rateLimitConfig
+                                        .setKeyResolver(userKeyResolver())
+                                        .setRateLimiter(redisRateLimiter())))
                         .uri("lb://CARDS"))
                 .route("loans", r -> r
                         .path("/idealbank/loans/**")
@@ -55,5 +61,17 @@ public class GatewayConfig {
                         .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
                         .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
                         .build());
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono
+                .justOrEmpty(exchange.getRequest().getHeaders().getFirst("X-User"))
+                .defaultIfEmpty("anonymous");
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter(){
+        return new RedisRateLimiter(1,1,1);
     }
 }
