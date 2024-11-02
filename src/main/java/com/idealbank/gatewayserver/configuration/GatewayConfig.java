7c76b0f7aada2af9f6d3e26aa70d58ebebc58a65
vivekkarli <vivekkarli7@gmail.com>
2024-11-02 14:02:10 +0530

    override def timeout for circuit breaker

--- src/main/java/com/idealbank/gatewayserver/configuration/GatewayConfig.java
+++ src/main/java/com/idealbank/gatewayserver/configuration/GatewayConfig.java
@@ -1,5 +1,10 @@
 package com.idealbank.gatewayserver.configuration;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -41,5 +46,14 @@ public class GatewayConfig {
                                         .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
                         .uri("lb://LOANS"))
                 .build();
+    }
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> slowCustomizer() {
+        return factory -> factory
+                .configureDefault(id -> new Resilience4JConfigBuilder(id)
+                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+                        .build());
     }
 }
